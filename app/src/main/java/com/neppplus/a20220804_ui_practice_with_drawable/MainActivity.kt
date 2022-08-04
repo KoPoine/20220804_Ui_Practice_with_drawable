2458c882633407f3e1437d6b8fc5cb259a28eec5
넵플러스 <kmc901206@gmail.com>
2022-08-04 17:18:10 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/a20220804_ui_practice_with_drawable/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.a20220804_ui_practice_with_drawable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
